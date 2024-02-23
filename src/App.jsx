703d785c7063c7fa8aa6f210e1b6d85a69cd0faa
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-23 18:49:04 +0530

    project setup done successfully🚀

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-slate-500'>Test</h1>
+      <h2 className='bg-slate-800'>Test 2</h2>
+    </>
+  )
+}
+
+export default App
