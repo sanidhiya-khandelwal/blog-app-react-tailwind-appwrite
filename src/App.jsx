b4a21128ce8e6dbcf579540bec444850289e5a8f
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-23 19:36:14 +0530

    appwrite and env variables setUp done successfully🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
+import conf from './conf/conf'
 
 function App() {
-
   return (
+
     <>
       <h1 className='bg-slate-500'>Test</h1>
       <h2 className='bg-slate-800'>Test 2</h2>
