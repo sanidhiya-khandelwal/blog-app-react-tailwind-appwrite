c2b74d4f9433f3f9bf3cbd5a883cf776234ad430
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-24 19:25:02 +0530

    checking user login or not during page load and added partial routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 import conf from './conf/conf'
-
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Footer, Header } from './components/index'
+import { Outlet } from 'react-router-dom';
 function App() {
-  return (
 
-    <>
-      <h1 className='bg-slate-500'>Test</h1>
-      <h2 className='bg-slate-800'>Test 2</h2>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(
+        () => setLoading(false)
+      )
+  }, [])
+  return !loading ? (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  ) : (
+    <div>
+      <h1>loading....</h1>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: []
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
