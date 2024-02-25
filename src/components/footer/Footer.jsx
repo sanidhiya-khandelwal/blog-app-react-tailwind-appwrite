3afbf0640a1a771753ef67c64d01a34eaf80a062
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-25 20:16:34 +0530

    made changes in footer styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Container from '../container/Container'
 function Footer() {
     return (
-        <Container>
+        <Container className="">
             <footer className='bg-gray-400 py-2 px-1 fixed bottom-0 left-10 right-10'>
-                <div className='flex flex-col gap-4 text-[0.8rem] sm:text-[0.9rem] sm:flex-row justify-between text-gray-700'>
+                <div className='flex flex-row flex-wrap gap-4 text-[0.8rem] sm:text-[0.9rem] justify-between text-gray-700'>
                     <ul>
                         <li className='cursor-pointer hover:underline hover:underline-offset-2'>About us</li>
                         <li className='cursor-pointer hover:underline hover:underline-offset-2'>Contact us</li>
@@ -26,7 +26,7 @@ function Footer() {
                         <li className='cursor-pointer hover:underline hover:underline-offset-2'>Press Kit</li>
                     </ul>
                 </div>
-                <h2 className='text-center mt-10'>Copywrite 2024</h2>
+                <h2 className='text-center mt-10 font-bold text-[0.9rem]'>Copywrite 2024</h2>
             </footer>
         </Container>
     )
