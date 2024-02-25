2d2dc5d66d45d7a03027b875ff6b966da09aef0c
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-25 19:54:11 +0530

    added class name varibale

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Container({ children, className = "" }) {
-    return <div className={`max-w-[1340px] m-auto px-4`} > {children}</div>
+    return <div className={`max-w-[1340px] m-auto px-4 ${className}`} > {children}</div>
 }
 
 export default Container
