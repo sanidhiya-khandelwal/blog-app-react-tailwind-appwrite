9c9310eb264c9198e8c6fc77dd4baa4e54c42ba8
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-28 18:28:29 +0530

    removed unwanted semi colon

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect } from 'react';
 import service from '../../appwrite/config';
-import { Input, Button, Select, RTE } from '../index';
+import { Input, Button, Select, RTE, Container } from '../index';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -79,59 +79,61 @@ function PostForm({ post }) { //log post
     }, [watch, slugTransform, setValue])
 
     return (
-        <form onSubmit={handleSubmit(submit)} className='flex flex-wrap'>
-            <div className='w-2/3 px-2'>
-                <Input
-                    label="Title : "
-                    placeholder="Enter title"
-                    className='mb-2'
-                    {...register("title", { required: true })}
-                />
-                <Input
-                    label="Slug : "
-                    placeholder="Slug"
-                    className='mb-2'
-                    {...register("slug", { required: true })}
-                    onInput={(e) => {
-                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true })
-                    }}
-                />
-                <RTE label="Description : " name="content" control={control} defaultValue={getValues("content")} />
-            </div>
-            <div className='w-1/3 px-2'>
-                <Input
-                    label="Featured Image : "
-                    type="file"
-                    className="mb-4"
-                    accept="image/png, image/jpg, image/jpeg, image/gif"
-                    {...register("image", { required: !post })} //for updating existing post it is not required but for new post(post will be empty so !empty = true) it is required
-                />
-                {post && (
-                    <div className="w-full mb-4">
-                        <img
-                            src={service.getFilePreview(post.featuredImage)}
-                            alt={post.title}
-                            className="rounded-lg"
-                        />
-                    </div>
-                )}
-                <Select
-                    options={["Personal", "Lifestyle", "Fashion", "Travel", "Food"]}
-                    label="Category :  : "
-                    className="mb-10"
-                    {...register("blogCategory", { required: true })}
-                />
-                <Select
-                    options={["active", "inactive"]}
-                    label="Status : "
-                    className="mb-4"
-                    {...register("status", { required: true })}
-                />
-                <Button type='submit' bgColor={post ? "bg-green-500" : undefined} className='w-full'>
-                    {post ? "Update" : "Submit"}
-                </Button>
-            </div>
-        </form>
+        <Container className='py-5'>
+            <form onSubmit={handleSubmit(submit)} className='flex flex-wrap border py-5'>
+                <div className='w-2/3 px-2'>
+                    <Input
+                        label="Title : "
+                        placeholder="Enter title"
+                        className='mb-2'
+                        {...register("title", { required: true })}
+                    />
+                    <Input
+                        label="Slug : "
+                        placeholder="Slug"
+                        className='mb-2'
+                        {...register("slug", { required: true })}
+                        onInput={(e) => {
+                            setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true })
+                        }}
+                    />
+                    <RTE label="Description : " name="content" control={control} defaultValue={getValues("content")} />
+                </div>
+                <div className='w-1/3 px-2'>
+                    <Input
+                        label="Featured Image : "
+                        type="file"
+                        className="mb-4"
+                        accept="image/png, image/jpg, image/jpeg, image/gif"
+                        {...register("image", { required: !post })} //for updating existing post it is not required but for new post(post will be empty so !empty = true) it is required
+                    />
+                    {post && (
+                        <div className="w-full mb-4">
+                            <img
+                                src={service.getFilePreview(post.featuredImage)}
+                                alt={post.title}
+                                className="rounded-lg"
+                            />
+                        </div>
+                    )}
+                    <Select
+                        options={["personal", "lifestyle", "fashion", "travel", "food"]}
+                        label="Category : "
+                        className="mb-10"
+                        {...register("blogCategory", { required: true })}
+                    />
+                    <Select
+                        options={["active", "inactive"]}
+                        label="Status : "
+                        className="mb-4"
+                        {...register("status", { required: true })}
+                    />
+                    <Button type='submit' bgColor={post ? "bg-green-500" : undefined} className='w-full'>
+                        {post ? "Update" : "Submit"}
+                    </Button>
+                </div>
+            </form>
+        </Container>
     )
 }
 
