6630a6167e8647348cf3919b778879e72d615a02
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-28 12:51:43 +0530

    fix: fixed how to see images in src=""

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import service from '../appwrite/config'
 function PostCard({ $id, title, featuredImage, blogCategory }) {
     return (
         <Link to={`/post/${$id}`}>
             <div className='mt-8 mb-2 rounded-md shadow-xl hover:scale-[1.01] duration-200 border-black w-[14rem] h-[16rem] sm:px-0 sm:w-[14rem] sm:h-[17rem]'>
-                <img className='object-cover px-1 w-full h-[85%] rounded-md' src={featuredImage} alt={title} />
+                <img className='object-cover px-1 w-full h-[85%] rounded-md' src={service.getFilePreview(featuredImage)} alt={title} />
                 <div className='flex justify-between px-3 mt-3'>
                     <h1 className='font-semibold'>{title}</h1>
                     <h6 className='font-semibold'>{blogCategory}</h6>
