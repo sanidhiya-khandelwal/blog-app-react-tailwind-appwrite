53a4ddc5302dd6a339339345edce383ad4cfd927
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-28 12:44:41 +0530

    fix: fixed onEditorChange

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -46,7 +46,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                             toolbar: "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
                             content_style: "body { font - family:Helvetica,Arial,sans-serif; font-size:14px }"
                         }}
-                        onChange={onChange}
+                        onEditorChange={onChange}
                     />
                 )}
             />
