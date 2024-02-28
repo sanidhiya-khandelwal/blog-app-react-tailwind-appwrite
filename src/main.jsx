062c0b1eebbd5dace37e9ab85a32edc2851dfad2
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-28 11:47:18 +0530

    loginPage, SignUpPag created and routes defined

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthLayout } from './components/index.js'
+import { HomePage, LoginPage, SignupPage } from './pages/pagesIndex.js'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: []
+    children: [
+      {
+        path: "/",
+        element: <HomePage />
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <LoginPage />
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <SignupPage />
+          </AuthLayout>
+        )
+      }
+    ]
   },
 ])
 
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Signup } from '../components/index'
+function SignupPage() {
+    return (
+        <div>
+            <Signup />
+        </div>
+    )
+}
+
+export default SignupPage
