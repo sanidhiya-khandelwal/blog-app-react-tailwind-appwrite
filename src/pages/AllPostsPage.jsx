5c5282ad4e292d641f6a5eb61696b8171267273d
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-29 19:10:09 +0530

    added loader

--- src/pages/AllPostsPage.jsx
+++ src/pages/AllPostsPage.jsx
@@ -12,15 +12,29 @@ function AllPostsPage() {
             }
         })
     }, [])
+
+    //waiting for the page to fetch data
+    if (posts.length === 0) {
+        return (
+            <div className='flex items-center justify-center'>
+                <img className='m-auto mt-5 h-full' src="./src/assets/spinner.gif" alt="spinner" />
+            </div>
+        )
+    }
+
     return (
         <Container>
             <div className='flex flex-wrap justify-around sm:justify-between gap-5 sm:gap-10'>
                 {
-                    posts.map(post => (
+                    posts ? (posts.map(post => (
                         <div key={post.$id}>
                             <PostCard {...post} />
+                        </div>)
+                    )) : (
+                        <div className='w-full h-[70vh] flex justify-center items-center'>
+                            <h1 className='font-semibold text-xl sm:text-2xl'>No posts created😑</h1>
                         </div>
-                    ))
+                    )
                 }
             </div>
         </Container>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,17 +6,19 @@ function HomePage() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        service.getAllPosts([]).then(posts => {
-            if (posts) {
-                setPosts(posts.documents)
-            }
-        })
+        service.getAllPosts([])
+            .then(posts => {
+                if (posts) {
+                    setPosts(posts.documents)
+                }
+            })
     }, [])
 
+    //waiting for the page to fetch data
     if (posts.length === 0) {
         return (
             <div className='flex items-center justify-center'>
-                <h1>No posts created...</h1>
+                <img className='m-auto mt-5 h-full' src="./src/assets/spinner.gif" alt="spinner" />
             </div>
         )
     }
@@ -24,11 +26,15 @@ function HomePage() {
         <Container>
             <div className='flex flex-wrap justify-around sm:justify-between gap-5 sm:gap-10'>
                 {
-                    posts.map(post => (
+                    posts ? (posts.map(post => (
                         <div key={post.$id}>
                             <PostCard {...post} />
+                        </div>)
+                    )) : (
+                        <div className='w-full h-[70vh] flex justify-center items-center'>
+                            <h1 className='font-semibold text-xl sm:text-2xl'>No posts created😑</h1>
                         </div>
-                    ))
+                    )
                 }
             </div>
         </Container>
